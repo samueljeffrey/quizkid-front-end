eb5a32c4f12c9d824c237ac180d3ddb38ad1d429
Samuel Jeffrey <samuel.jeffrey@outlook.com>
2022-03-02 13:30:11 +0000

    add getQuizzes to util functions

--- src/Utils/utils.tsx
+++ src/Utils/utils.tsx
@@ -1,1 +1,13 @@
 import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://samueljeffrey-quizkid.herokuapp.com/api",
+});
+
+export const getQuizzes = (category?: string) => {
+  let path: string = "/quizzes";
+  if (category !== "All") path += `?category=${category}`;
+  return api.get(path).then((response) => {
+    return response;
+  });
+};
